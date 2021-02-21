863a1765a267ea4ac490328413b5e1d2d836d853
Uma Manandhar <um_2003_8@hotmail.com>
2021-02-21 03:19:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function findMinAndRemove(array){
-
+  let minElement = array[0]
+  let minIndex = 0
+  for (let i = 0; i < array.length; i++) {
+    if(array[i] < minElement) {
+      minElement = array[i]
+      minIndex = i
+    }
+  }
+  array.splice(minIndex, 1)
+  return minElement
 }
 
 function insertionSort(array){
-
+  let minimum
+  let sortedArray = []
+  while (array.length != 0) {
+    minimum = findMinAndRemove(array)
+    sortedArray.push(minimum)
+  }
+  return sortedArray
 }
