29b5620cd1b2792026202f111706535e1f23dc9c
Uma Manandhar <um_2003_8@hotmail.com>
2021-02-21 03:18:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,13 +20,3 @@ function insertionSort(array){
   }
   return sortedArray
 }
-
-//let array = [2, 3, 4, 5, 6, 7]
-//find the minimum element
-//grab the first element then compare it to 2nd element if its greater or less than first
-//remove the one which is smaller
-//return minimum element
-
-// let unsorted = [12, 10, 9, 14, 1, 3, 5, 11, 6, 15, 16, 13, 2, 4, 8, 7]
-// create variable minimum and empty array to store minimum element
-//
