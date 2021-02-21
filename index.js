8203cc856b335d4fba86e452dc3f74a91b5bab24
Uma Manandhar <um_2003_8@hotmail.com>
2021-02-21 03:01:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function findMinAndRemove(array){
-
+  let minElement = array[0]
+  let minIndex = 0
+  for (let i = 0; i < array.length; i++) {
+    if(array[i] < minElement) {
+      minElement = array[i]
+      minIndex = i 
+    }
+  }
+  array.splice(minIndex, 1)
+  return minElement
 }
 
 function insertionSort(array){
 
 }
+
+//find the minimum element
+//grab the first element then compare it to 2nd element if its greater or less than first
+//remove the one which is greater
+//repeate it until small element left in the array
+//return minimum element
